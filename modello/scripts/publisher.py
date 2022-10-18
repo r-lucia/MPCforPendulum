ab4eb7b13318c39ff5356d39ae5d6259706ee2a8
Lucia <luciariccio97@gmail.com>
2022-10-18 19:32:40 +0200

    nodo che impone posizione del carrello e legge angolo del pendolo

--- modello/scripts/publisher.py
+++ modello/scripts/publisher.py
@@ -2,16 +2,25 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import Float64
+from sensor_msgs.msg import JointState
+
+def callback(msg):
+    global teta_pendolo
+    teta_pendolo= msg.position[1]
+    rospy.loginfo(rospy.get_caller_id() + "I heard %f", teta_pendolo)
 
 def talker():
     pub = rospy.Publisher('pend_carr_controller/command', Float64, queue_size=10)
+    rospy.Subscriber("joint_states",JointState, callback)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         pos_desiderata = 0
-        rospy.loginfo(pos_desiderata)
+        rospy.loginfo( "posa desiderata %f", pos_desiderata)
         pub.publish(pos_desiderata)
+        rospy.logwarn(teta_pendolo)
         rate.sleep()
+        
 
 if __name__ == '__main__':
     try:
